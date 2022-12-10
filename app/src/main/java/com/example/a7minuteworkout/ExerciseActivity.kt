6cae6e27c2c19d6fa970ec1da5a3b34cb205cb36
patilrajvardhan27 <patilrajvardhan27@gmail.com>
2022-12-10 18:57:52 +0530

    new

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -2,10 +2,49 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import com.example.a7minuteworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExerciseBinding? = null
+
+    private var restTimer : CountDownTimer?=null
+    private var restProgress =0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
+
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress =restProgress
+
+        restTimer = object :CountDownTimer(10000,1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress =10- restProgress
+                binding?.tvTimer?.text = (10-restProgress).toString()
+
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                "Here we will start exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
     }
 }
